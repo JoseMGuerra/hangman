4a4e3fb5a246fd4b985542c4e315ab7bf5dec2fa
JoseMGuerra <Josem_guerra@hotmail.com>
2022-08-19 17:40:50 +0000

    Add helper functions, quit(), clear(), space(), pause()

--- run.py
+++ run.py
@@ -1,10 +1,48 @@
 # Imported modules
 import sys
+import time
+
+FILE_PATH = "assets/py/user_details.csv"
+
+
+def quit():
+    """
+    Helper function to exit the game
+    "sys.exit()"
+    """
+    sys.exit()
+
+
+def clear():
+    """
+    Clear the screen
+    "print('\\033c')"
+    """
+    print('\033c')
+
+
+def space():
+    """
+    Prints an empty line
+    "print("")"
+    """
+    print("")
+
+
+def pause(s):
+    """
+    Pause the screen for nth seconds
+    better user experience
+    "time.sleep(s)"
+    """
+    time.sleep(s)
+
 
 def register():
     """
     Get username and password input from the user, validates user inputs.
     add user to user_details file or creates new file if it doesn't exits.
+
     """
     pass
 
@@ -15,28 +53,18 @@ def login():
     """
     pass
 
-def quit():
-    """
-    Helper function to exit the game
-    "sys.exit()"
-    """
-    sys.exit()
 
 def main_menu():
     """
     Main menu, register and login.
-    
     """
     choice = ""
     while choice != "3":
         print("""
-        
         1. Press 1 to Register
         2. Press 2 to Login
         3. Press 3 to Quit
-        
         """)
-    
         choice = input()
 
         if choice == "1":
